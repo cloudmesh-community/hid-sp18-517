c2db212b06512e08bd7335381ab1f587dab35544
Harshad Pitkar <harshpitkar@gmail.com>
2018-02-17 14:14:17 -0500

    Swagger initial version

---
+++ swagger/server/lambda/flaskConnexion/swagger_server/controllers/default_controller.py
@@ -0,0 +1,16 @@
+import connexion
+import six
+
+from swagger_server.models.add import ADD  # noqa: E501
+from swagger_server import util
+
+
+def add_num():  # noqa: E501
+    """add_num
+
+    Returns addition result information of the hosting server # noqa: E501
+
+
+    :rtype: ADD
+    """
+    return 'do some magic!'
