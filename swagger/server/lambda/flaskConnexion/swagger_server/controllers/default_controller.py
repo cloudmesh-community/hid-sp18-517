1edb90531a145315a76bbe9bb073517101e2e320
harshad <harshpitkar@gmail.com>
2018-02-18 17:24:29 -0500

    added parameters

--- swagger/server/lambda/flaskConnexion/swagger_server/controllers/default_controller.py
+++ swagger/server/lambda/flaskConnexion/swagger_server/controllers/default_controller.py
@@ -5,16 +5,18 @@ from swagger_server.models.add import ADD  # noqa: E501
 from swagger_server import util
 
 
-def add_num():  # noqa: E501
+def add_num(number1, number2):  # noqa: E501
     """add_num
 
-    Returns addition result information of the hosting server # noqa: E501
+    Returns addition result from the hosting server # noqa: E501
 
+    :param number1: Number 1
+    :type number1: int
+    :param number2: Number 2
+    :type number2: int
 
     :rtype: ADD
     """
+    sum = number1 + number2
+    return sum
     #return 'do some magic!'
-    f = lambda x,y : x + y
-    add = f(1,3)
-    return add
-
