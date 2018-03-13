b92f3bac812c718c086eae420e360d4a5a8f1679
harshad <harshpitkar@gmail.com>
2018-03-13 14:32:28 -0400

    swagger updated

--- swagger/cloudmesh/fileupload/math.py
+++
@@ -1,4 +0,0 @@
-def math(number1, number2):  # noqa: E501
-    sum = number1 + number2
-    return sum
-
--- swagger/cloudmesh/fileupload/xmltojson.py
+++
@@ -1,9 +0,0 @@
-from json import dumps
-from xmljson import badgerfish as bf
-from xml.etree.ElementTree import fromstring
-
-def xml2json_conv(xmlStr):
-	return dumps(bf.data(fromstring(xmlStr)))
-
-
-# https://pypi.python.org/pypi/xmljson
